76cecb9f972b5709c2541879eb3ecd9a54666c93
PabloCaleroCuellar <p.calerocuellar@gmail.com>
2020-12-19 15:07:42 +0100

    Fix: button timer when move between components
        - Fixed the timer button: now when you change between components
          when the timer is running the button dont change to "Reanudar
          partido"

--- src/app/components/partido/partido.component.ts
+++ src/app/components/partido/partido.component.ts
@@ -4,6 +4,7 @@ import { DataService } from '../../service/data.service';
 
 let interval;
 let partidoAcabado: boolean = false;
+let isTimerOn: boolean = false;
 
 @Component({
   selector: 'app-partido',
@@ -28,6 +29,12 @@ export class PartidoComponent implements OnInit {
         botonTimer.setAttribute("class", "btn btn-warning")
         botonTimer.innerHTML = "Partido acabado";
     }
+    else {
+      if(isTimerOn){
+            botonTimer.setAttribute("class", "btn btn-danger")
+            botonTimer.innerHTML = "Parar tiempo";
+          }
+    }
   }
 
   basketLocal(value: number) {
@@ -50,6 +57,7 @@ export class PartidoComponent implements OnInit {
     }
   
     if (!interval) {
+      isTimerOn = true
       interval = setInterval(()=> {
         this.partido._segundos--;
         if(this.partido._segundos < 0) {
@@ -71,6 +79,7 @@ export class PartidoComponent implements OnInit {
             clearInterval(interval);
           }
           else {
+            isTimerOn = false
             this.partido._cuarto++;
             clearInterval(interval);
             if (botonTimer.innerHTML == "Reanudar partido") {
@@ -89,6 +98,7 @@ export class PartidoComponent implements OnInit {
       }, 1000);
     } 
     else {
+      isTimerOn = false
       clearInterval(interval);
       interval = null;
     }
--- src/app/model/partido.ts
+++ src/app/model/partido.ts
@@ -2,20 +2,18 @@ export class Partido {
     _equipoLocal : string;
     _equipoVisitante : string;
     _cuarto : number;
-    //_reloj : number;
     _minutos : number;
     _segundos : number
     _puntosLocal : number;
     _puntosVisitante : number;
 
-    constructor(equipoLocal: string, equipoVisitante : string, cuarto : number, minutos : number, segundos: number, /*reloj : number,*/
+    constructor(equipoLocal: string, equipoVisitante : string, cuarto : number, minutos : number, segundos: number, 
         puntosLocal : number, puntosVisitante : number) {
         this._equipoLocal = equipoLocal;
         this._equipoVisitante = equipoVisitante;
         this._cuarto = cuarto;
         this._minutos = minutos;
         this._segundos = segundos;
-        //this._reloj = reloj;
         this._puntosLocal = puntosLocal;
         this._puntosVisitante = puntosVisitante;
       }
